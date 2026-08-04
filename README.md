b74c615bfe8db7978dde002c0012357cf33b366c
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-08-04 20:24:16 +0200

    "Initial boylerplate"

---
+++ README.md
@@ -0,0 +1,3 @@
+# gg_one_merge
+
+Merge flow machinery of the gg_one tool family - merging feature branches via pull requests or local merges.
---
+++ lib/src/gg_one_merge.dart
@@ -0,0 +1,14 @@
+// @license
+// Copyright (c) 2019 - 2026 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
+
+/// The main class of the package
+class GgOneMerge {
+  /// Constructor
+  const GgOneMerge();
+
+  /// A sample method
+  String foo() => 'foo';
+}
